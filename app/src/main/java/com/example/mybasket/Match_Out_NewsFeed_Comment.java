9bacc40b444809b11e5b666ab44c2ab40f268fa3
parkHG1234 <ootu789@naver.com>
2016-08-05 15:37:45 +0900

    코트정보 네비게이션 구현

--- app/src/main/java/com/example/mybasket/Match_Out_NewsFeed_Comment.java
+++ app/src/main/java/com/example/mybasket/Match_Out_NewsFeed_Comment.java
@@ -8,8 +8,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.StrictMode;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.Button;
 import android.widget.EditText;
@@ -17,6 +27,8 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
@@ -32,6 +44,8 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +53,7 @@ import java.util.List;
 /**
  * Created by 박지훈 on 2016-06-27.
  */
-public class Match_Out_NewsFeed_Comment extends Activity implements AbsListView.OnScrollListener {
+public class Match_Out_NewsFeed_Comment extends AppCompatActivity implements AbsListView.OnScrollListener, NavigationView.OnNavigationItemSelectedListener {
 
     ImageView NewsFeed_Comment_Emblem;
     TextView NewsFeed_Comment_Court;
@@ -49,13 +63,12 @@ public class Match_Out_NewsFeed_Comment extends Activity implements AbsListView.
     ListView NewSpeed_Comment_List;
     EditText NewsFeed_Comment_EditText;
     Button NewsFeed_Comment_Button;
-    ImageView NewSpeed_Comment_ImageView;
 
 
     Match_Out_NewsFeed_Comment_Adapter CommentAdapter;
     ArrayList<Match_Out_NewsFeed_Comment_Setting> arrComment;
     String NewsFeed_Num;
-    String[][] parsedData;
+    String[][] parsedData,parsedData_CourtInfo;
     String[] jsonName = {"Comment_Num", "NewsFeed_Num", "Comment_Person", "Comment_Data", "Comment_Month", "Comment_Day", "Comment_Hour", "Comment_Minute"};
 
     Handler handler = new Handler();
@@ -71,7 +84,27 @@ public class Match_Out_NewsFeed_Comment extends Activity implements AbsListView.
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout_match_out_newsfeed_comment);
+        setContentView(R.layout.layout_123);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar123);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        final View aa = navigationView.inflateHeaderView(R.layout.layout_courtinfo_navheader);
+        final TextView CourtInfo_Nav_CourtName = (TextView) aa.findViewById(R.id.CourtInfo_Nav_CourtName);
+        final TextView CourtInfo_Nav_CourtFloor = (TextView) aa.findViewById(R.id.CourtInfo_Nav_CourtFloor);
+        final TextView CourtInfo_Nav_CourtAddress = (TextView) aa.findViewById(R.id.CourtInfo_Nav_CourtAddress);
+        final TextView CourtInfo_Nav_CourtCount = (TextView) aa.findViewById(R.id.CourtInfo_Nav_CourtCount);
+
+        final ImageView CourtInfo_Nav_Img1 = (ImageView)aa.findViewById(R.id.CourtInfo_Nav_Img1);
+        final ImageView CourtInfo_Nav_Img2 = (ImageView)aa.findViewById(R.id.CourtInfo_Nav_Img2);
+        final ImageView CourtInfo_Nav_Img3 = (ImageView)aa.findViewById(R.id.CourtInfo_Nav_Img3);
 
         NewsFeed_Comment_Emblem = (ImageView) findViewById(R.id.NewsFeed_Comment_Emblem);
         NewsFeed_Comment_Court = (TextView) findViewById(R.id.NewsFeed_Comment_Court);
@@ -82,8 +115,6 @@ public class Match_Out_NewsFeed_Comment extends Activity implements AbsListView.
         NewsFeed_Comment_EditText = (EditText) findViewById(R.id.NewsFeed_Comment_EditText);
         NewsFeed_Comment_Button = (Button) findViewById(R.id.NewsFeed_Comment_Button);
         NewsFeed_Comment_ProgressBar = (ProgressBar) findViewById(R.id.NewsFeed_Comment_ProgressBar);
-        NewSpeed_Comment_ImageView = (ImageView) findViewById(R.id.NewSpeed_Comment_ImageView);
-
 
         final Intent CommentIntent = getIntent();
         NewsFeed_Num = CommentIntent.getExtras().getString("Num");
@@ -92,6 +123,60 @@ public class Match_Out_NewsFeed_Comment extends Activity implements AbsListView.
         NewsFeed_Comment_Data.setText(CommentIntent.getExtras().getString("Data"));
         NewsFeed_Comment_Time.setText(CommentIntent.getExtras().getString("Time"));
 
+        String result_CourtInfo="";
+        try {
+            HttpClient client = new DefaultHttpClient();
+            String postURL = "http://210.122.7.195:8080/Web_basket/CourtInfo.jsp";
+            HttpPost post = new HttpPost(postURL);
+
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("CourtName", CommentIntent.getExtras().getString("Court")));
+
+            UrlEncodedFormEntity ent = new UrlEncodedFormEntity(params, HTTP.UTF_8);
+            post.setEntity(ent);
+
+            HttpResponse response = client.execute(post);
+            BufferedReader bufreader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "utf-8"));
+
+            String line = null;
+            while ((line = bufreader.readLine()) != null) {
+                result_CourtInfo += line;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        parsedData_CourtInfo = jsonParserList_CourtInfo(result_CourtInfo);
+        String CourtName =parsedData_CourtInfo[0][0];
+        String CourtAddress =parsedData_CourtInfo[0][1];
+        String CourtCount =parsedData_CourtInfo[0][2];
+        String CourtFloor =parsedData_CourtInfo[0][3];
+        String CourtMapX =parsedData_CourtInfo[0][4];
+        String CourtMapy =parsedData_CourtInfo[0][5];
+        String Image1 =parsedData_CourtInfo[0][6];
+        String Image2 =parsedData_CourtInfo[0][7];
+        String Image3 =parsedData_CourtInfo[0][8];
+
+        CourtInfo_Nav_CourtName.setText(CourtName);
+        CourtInfo_Nav_CourtAddress.setText(CourtAddress);
+        CourtInfo_Nav_CourtCount.setText(CourtCount);
+        CourtInfo_Nav_CourtFloor.setText(CourtFloor);
+//URI 한글 인코딩
+        try{
+            String En_Image1 = URLEncoder.encode(Image1, "utf-8");
+            String En_Image2 = URLEncoder.encode(Image2, "utf-8");
+            String En_Image3 = URLEncoder.encode(Image3, "utf-8");
+
+            Glide.with(Match_Out_NewsFeed_Comment.this).load("http://210.122.7.195:8080/Web_basket/imgs/Court/"+En_Image1+".jpg")
+                    .into(CourtInfo_Nav_Img1);
+            Glide.with(Match_Out_NewsFeed_Comment.this).load("http://210.122.7.195:8080/Web_basket/imgs/Court/"+En_Image2+".jpg")
+                    .into(CourtInfo_Nav_Img2);
+            Glide.with(Match_Out_NewsFeed_Comment.this).load("http://210.122.7.195:8080/Web_basket/imgs/Court/"+En_Image3+".jpg")
+                    .into(CourtInfo_Nav_Img3);
+
+        }catch (UnsupportedEncodingException e){
+
+        }
         String result = "";
         try {
             HttpClient client = new DefaultHttpClient();
@@ -196,7 +281,26 @@ public class Match_Out_NewsFeed_Comment extends Activity implements AbsListView.
             return null;
         }
     }
+    public String[][] jsonParserList_CourtInfo(String pRecvServerPage){
+        Log.i("서버에서 받은 전체 내용", pRecvServerPage);
+        try{
+            JSONObject json = new JSONObject(pRecvServerPage);
+            JSONArray jArr = json.getJSONArray("List");
 
+            String[] jsonName = {"msg1","msg2","msg3","msg4","msg5","msg6","msg7","msg8","msg9"};
+            String[][] parseredData = new String[jArr.length()][jsonName.length];
+            for(int i = 0; i<jArr.length();i++){
+                json = jArr.getJSONObject(i);
+                for (int j=0;j<jsonName.length; j++){
+                    parseredData[i][j] = json.getString(jsonName[j]);
+                }
+            }
+            return parseredData;
+        }catch (JSONException e){
+            e.printStackTrace();
+            return null;
+        }
+    }
     @Override
     public void onScrollStateChanged(AbsListView absListView, int scrollState) {
 
@@ -207,13 +311,7 @@ public class Match_Out_NewsFeed_Comment extends Activity implements AbsListView.
             }  else{
                 cnt = cnt + 10;
             }
-            final Handler handler = new Handler();
-            handler.postDelayed(new Runnable() {
-                @Override
-                public void run() {
-                    NewsFeed_Comment_ProgressBar.setVisibility(View.GONE);
-                }
-            }, 1000);
+
             try {
                 for (int i = 0; i < cnt; i++) {
                     json = jArr.getJSONObject(i);
@@ -229,7 +327,7 @@ public class Match_Out_NewsFeed_Comment extends Activity implements AbsListView.
             CommentAdapter.listview(NewSpeed_Comment_List);
             NewSpeed_Comment_List = (ListView) findViewById(R.id.NewSpeed_Comment_List);
             NewSpeed_Comment_List.setAdapter(CommentAdapter);
-//            NewSpeed_Comment_List.setSelection(pos + 1);
+            NewSpeed_Comment_List.setSelection(pos + 1);
 
         }
     }
@@ -238,5 +336,36 @@ public class Match_Out_NewsFeed_Comment extends Activity implements AbsListView.
     public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
         VisibleFlag = (totalItemCount > 0) && (firstVisibleItem + visibleItemCount >= totalItemCount);
         pos = totalItemCount - visibleItemCount;
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
