65c8c4f79743814f89af1a14cd14f5b5c7398e34
ldong <hccs611@gmail.com>
2016-08-30 13:48:25 +0900

    rr

--- app/src/main/java/com/example/mybasket/JoinAddActivity.java
+++ app/src/main/java/com/example/mybasket/JoinAddActivity.java
@@ -8,10 +8,12 @@ import android.os.StrictMode;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import org.apache.http.HttpResponse;
@@ -22,6 +24,10 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
@@ -37,7 +43,14 @@ public class JoinAddActivity extends Activity{
 
     static String id, pw, user_type;
     RadioGroup join_sex_Radio, join_posi_Radio;
-    EditText join_name_EditText, join_bYear_EditText, join_bMonth_EditText, join_bDay_EditText,join_layout_weight_editText,join_layout_height_editText;
+    EditText join_name_EditText,join_layout_weight_editText,join_layout_height_editText;
+    String[][] parsedData;
+
+    TextView tvBirth;
+
+    Spinner s_Sex;
+    Spinner s_Position;
+
     int year, month, day, hour, minute;
 
     @Override
@@ -52,6 +65,9 @@ public class JoinAddActivity extends Activity{
         pw = intentGet.getStringExtra("pw");
         user_type = intentGet.getStringExtra("user_type");
 
+        final String birthTemp = "";
+        String posiTemp = "";
+
 
         GregorianCalendar calendar = new GregorianCalendar();
         year = calendar.get(Calendar.YEAR);
@@ -60,13 +76,31 @@ public class JoinAddActivity extends Activity{
         hour = calendar.get(Calendar.HOUR_OF_DAY);
         minute = calendar.get(Calendar.MINUTE);
 
-        Spinner s_Sex = (Spinner)findViewById(R.id.spinner_sex);
-        Spinner s_Position = (Spinner)findViewById(R.id.spinner_position);
+        tvBirth = (TextView) findViewById(R.id.join_layout_birth_tv);
+
+        s_Sex = (Spinner)findViewById(R.id.spinner_sex);
+        s_Position = (Spinner)findViewById(R.id.spinner_position);
+
+        findViewById(R.id.join_layout_birth_btn).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                new DatePickerDialog(JoinAddActivity.this, dateSetListener, year, month, day).show();
+            }
+        });
+
+
+
 
         s_Sex.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
+                if(position==0) {
+
+                }else if(position==1) {
+
+                }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {}
@@ -76,6 +110,17 @@ public class JoinAddActivity extends Activity{
             @Override
             public void onItemSelected(AdapterView<?> parent, View view,
                                        int position, long id) {
+                if(position==0) {
+
+                }else if(position==1) {
+
+                }else if(position==2) {
+
+                }else if(position==3) {
+
+                }else if(position==4) {
+
+                }
             }
             @Override
             public void onNothingSelected(AdapterView<?> parent) {}
@@ -89,66 +134,101 @@ public class JoinAddActivity extends Activity{
 
     }
 
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear,
+                              int dayOfMonth) {
+            // TODO Auto-generated method stub
+            String msg = String.format("%d / %d / %d", year,monthOfYear+1, dayOfMonth);
+            tvBirth.setText(msg);
+
+            Toast.makeText(JoinAddActivity.this, msg, Toast.LENGTH_SHORT).show();
+        }
+    };
+
+
+
     public void onClickCommit(View view) {
         String name = join_name_EditText.getText().toString();
-        String bYear = join_bYear_EditText.getText().toString();
-        String bMonth = join_bMonth_EditText.getText().toString();
-        String bDay = join_bDay_EditText.getText().toString();
-        String birth = bYear +"/"+bMonth+"/"+bDay;
+        String birth = tvBirth.getText().toString();
         String height = join_layout_height_editText.getText().toString();
         String weight = join_layout_weight_editText.getText().toString();
 
-        //라디오 버튼에서 성별값 받아옴
-        int sex_id = join_sex_Radio.getCheckedRadioButtonId();
-        RadioButton sex_radio = (RadioButton) findViewById(sex_id);
-        String sex = sex_radio.getText().toString();
+        String sex = s_Sex.getSelectedItem().toString();
+        String posi = s_Position.getSelectedItem().toString();
 
-        //라디오 버튼에서 포지션 값 받아옴
-        int posi_id = join_posi_Radio.getCheckedRadioButtonId();
-        RadioButton posi_radio = (RadioButton) findViewById(posi_id);
-        String posi = posi_radio.getText().toString();
+        Toast.makeText(JoinAddActivity.this, "height = "+height, Toast.LENGTH_LONG).show();
+        String result = JoinByHttp(id,pw,user_type,name,sex,birth,posi,weight,height);
+        parsedData = jsonParserList(result);
 
-        Toast.makeText(JoinAddActivity.this, "id = "+id, Toast.LENGTH_LONG).show();
-        JoinByHttp(id,pw,user_type,name,sex,birth,posi,weight,height);
         //joinThread jt = new joinThread();
         //jt.run();
 
     }
-        private String JoinByHttp(String _id, String _pw, String user_type, String _name, String _sex,String _birth, String _posi, String _weight, String _height) {
-            try {
-                HttpClient client = new DefaultHttpClient();
-                String url = "http://210.122.7.195:8080/Web_basket/Join.jsp";
-                HttpPost post = new HttpPost(url);
 
-                List<NameValuePair> params = new ArrayList<NameValuePair>();
-                params.add(new BasicNameValuePair("_id", _id));
-                params.add(new BasicNameValuePair("_pw", _pw));
-                params.add(new BasicNameValuePair("_user_type", user_type));
-                params.add(new BasicNameValuePair("_name", _name));
-                params.add(new BasicNameValuePair("_sex", _sex));
-                params.add(new BasicNameValuePair("_birth", _birth));
-                params.add(new BasicNameValuePair("_posi", _posi));
-                params.add(new BasicNameValuePair("_weight", _weight));
-                params.add(new BasicNameValuePair("_height", _height));;
 
-                UrlEncodedFormEntity ent = new UrlEncodedFormEntity(params, HTTP.UTF_8);
-                post.setEntity(ent);
 
-                HttpResponse response = client.execute(post);
-                BufferedReader bufreader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "utf-8"));
+    private String JoinByHttp(String _id, String _pw, String _user_type, String _name, String _sex,String _birth, String _posi, String _weight, String _height) {
+        try {
+            HttpClient client = new DefaultHttpClient();
+            String url = "http://210.122.7.195:8080/pp/Join.jsp";
+            HttpPost post = new HttpPost(url);
 
-                String line = null;
-                String result = "";
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("_id", _id));
+            params.add(new BasicNameValuePair("_pw", _pw));
+            params.add(new BasicNameValuePair("user_type", _user_type));
+            params.add(new BasicNameValuePair("_name", _name));
+            params.add(new BasicNameValuePair("_sex", _sex));
+            params.add(new BasicNameValuePair("_birth", _birth));
+            params.add(new BasicNameValuePair("_posi", _posi));
+            params.add(new BasicNameValuePair("_weight", _weight));
+            params.add(new BasicNameValuePair("_height", _height));;
 
-                while ((line = bufreader.readLine()) != null) {
-                    result += line;
-                }
-                return result;
-            } catch (Exception e) {
-                e.printStackTrace();
-                return "";
+            UrlEncodedFormEntity ent = new UrlEncodedFormEntity(params, HTTP.UTF_8);
+            post.setEntity(ent);
+
+            HttpResponse response = client.execute(post);
+            BufferedReader bufreader = new BufferedReader(new InputStreamReader(response.getEntity().getContent(), "utf-8"));
+
+            String line = null;
+            String result = "";
+
+            while ((line = bufreader.readLine()) != null) {
+                result += line;
             }
-
+            return result;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return "";
+        }
     }
 
+    private String[][] jsonParserList(String pRecvServerPage) {
+        Log.i("서버에서 받은 전체 내용", pRecvServerPage);
+        try {
+            JSONObject json = new JSONObject(pRecvServerPage);
+            JSONArray jarr = json.getJSONArray("List");
+
+            //String[] jsonName = {"login_check","usercode","password","name","sex","email","univ","club" };
+
+            String[] jsonName = {"msg1"};
+            String[][] parseredData = new String[jarr.length()][jsonName.length];
+            for(int i = 0; i<jarr.length();i++){
+                json = jarr.getJSONObject(i);
+                for (int j=0;j<jsonName.length; j++){
+                    parseredData[i][j] = json.getString(jsonName[j]);
+                }
+            }
+            for(int i=0;i<parseredData.length;i++) {
+                Log.i("JSON을 분석한 데이터" + i + ":", parseredData[i][0]);
+            }
+            return parseredData;
+        }catch(JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
 }
