c2cca1d0b73db007d13ab7ec1fa50ca99772ea79
parkHG1234 <ootu789@naver.com>
2016-09-20 18:40:41 +0900

    fcm 알림 구현 및 일부 디자인 수정

--- app/src/main/java/com/example/mybasket/Match_In_Focus.java
+++ app/src/main/java/com/example/mybasket/Match_In_Focus.java
@@ -257,19 +257,19 @@ public class Match_In_Focus extends AppCompatActivity {
             String En_Image2 = URLEncoder.encode(Image2, "utf-8");
             String En_Image3 = URLEncoder.encode(Image3, "utf-8");
             if (Image1.equals(".")) {
-                Glide.with(Match_In_Focus.this).load(R.drawable.profile_basic_image).into(Match_In_Focus_ImageVIew_TeamImageView1);
+                Match_In_Focus_ImageVIew_TeamImageView1.setVisibility(View.GONE);
             }
             else{
                 Glide.with(Match_In_Focus.this).load("http://210.122.7.195:8080/Web_basket/imgs/Team/" + En_Image1 + ".jpg").into(Match_In_Focus_ImageVIew_TeamImageView1);
             }
             if (Image2.equals(".")) {
-                Glide.with(Match_In_Focus.this).load(R.drawable.profile_basic_image).into(Match_In_Focus_ImageVIew_TeamImageView2);
+                Match_In_Focus_ImageVIew_TeamImageView2.setVisibility(View.GONE);
             }
             else{
                 Glide.with(Match_In_Focus.this).load("http://210.122.7.195:8080/Web_basket/imgs/Team/" + En_Image2 + ".jpg").into(Match_In_Focus_ImageVIew_TeamImageView2);
             }
             if (Image3.equals(".")) {
-                Glide.with(Match_In_Focus.this).load(R.drawable.profile_basic_image).into(Match_In_Focus_ImageVIew_TeamImageView3);
+                Match_In_Focus_ImageVIew_TeamImageView3.setVisibility(View.GONE);
             }
             else{
                 Glide.with(Match_In_Focus.this).load("http://210.122.7.195:8080/Web_basket/imgs/Team/" + En_Image3 + ".jpg").into(Match_In_Focus_ImageVIew_TeamImageView3);
