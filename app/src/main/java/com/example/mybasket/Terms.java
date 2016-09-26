aa83a91f202307524b4836f96a3b8bf0f74a0ee0
ldong <hccs611@gmail.com>
2016-09-26 23:40:58 +0900

    자동로그인, 약관 스크롤뷰,

--- app/src/main/java/com/example/mybasket/Terms.java
+++ app/src/main/java/com/example/mybasket/Terms.java
@@ -2,6 +2,9 @@ package com.example.mybasket;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 /**
  * Created by ldong on 2016-09-22.
@@ -11,5 +14,13 @@ public class Terms extends Activity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.terms);
 
+
+        WebView WebView01 = (WebView) findViewById(R.id.webview01);
+        WebView01.setWebViewClient(new WebViewClient());
+
+        WebSettings webSettings = WebView01.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        WebView01.loadUrl("http://210.122.7.195:8080/pp/term.jsp");
     }
 }
