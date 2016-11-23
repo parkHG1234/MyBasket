311311b199a3b7e0431529bff6683f2fa23b2ad5
ldong <hccs611@gmail.com>
2016-11-23 10:45:18 +0900

    대회 상세 보기 완료

--- app/src/main/java/com/mysports/basketbook/Contests_Customlist_Adapter.java
+++ app/src/main/java/com/mysports/basketbook/Contests_Customlist_Adapter.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -21,6 +22,7 @@ public class Contests_Customlist_Adapter extends BaseAdapter{
     private ArrayList<Contests_Customlist_MyData> arrData;
     private LayoutInflater inflater;
     String Contest_Pk;
+    private int position2;
     public Contests_Customlist_Adapter(Context c, ArrayList<Contests_Customlist_MyData> arr) {
         this.context = c;
         this.arrData = arr;
@@ -43,11 +45,11 @@ public class Contests_Customlist_Adapter extends BaseAdapter{
     }
 
     @Override
-    public View getView(int position, View convertView, ViewGroup parent) {
+    public View getView(final int position, View convertView, ViewGroup parent) {
         if(convertView == null) {
             convertView = inflater.inflate(R.layout.layout_customlist_contests, parent, false);
 
-
+            position2 = position;
             String Contest_Title = arrData.get(position).getContest_Title();
             String Contest_Image = arrData.get(position).getContest_Image();
             String Contest_Date = arrData.get(position).getContest_Date();
@@ -55,23 +57,25 @@ public class Contests_Customlist_Adapter extends BaseAdapter{
             String Contest_maxNum = arrData.get(position).getContest_maxNum();
             String Contest_Point = arrData.get(position).getContest_Point();
             Contest_Pk = arrData.get(position).getContest_Pk();
+            Log.i("Pk1",Contest_Pk);
 
             ImageView viewContest_Image = (ImageView)convertView.findViewById(R.id.Contest_logoImage);
             TextView viewContest_Title = (TextView)convertView.findViewById(R.id.contest_title);
             TextView viewContest_Date = (TextView)convertView.findViewById(R.id.contest_date);
             TextView viewContest_Num = (TextView)convertView.findViewById(R.id.contest_num);
             TextView viewContest_Point = (TextView)convertView.findViewById(R.id.contest_point);
+            final LinearLayout Contest_Layout = (LinearLayout)convertView.findViewById(R.id.Contest_Layout);
 
             viewContest_Title.setText(Contest_Title);
             viewContest_Date.setText(Contest_Date);
             viewContest_Num.setText(Contest_currentNum + " / " + Contest_maxNum);
             viewContest_Point.setText(Contest_Point);
-            convertView.setOnClickListener(new View.OnClickListener() {
+            Contest_Layout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
-                    Log.i("Pk",Contest_Pk);
+                    Log.i("Pk",arrData.get(position).getContest_Pk());
                     Intent intent = new Intent(context, Contest_Detail.class);
-                    intent.putExtra("position", Contest_Pk);
+                    intent.putExtra("position", arrData.get(position).getContest_Pk());
                     intent.putExtra("Id", MainActivity.Id);
                     context.startActivity(intent);
                 }
